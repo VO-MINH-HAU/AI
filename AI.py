daf558740acd0a0c9dd5942edb75aacf4ca9cde2
VO-MINH-HAU <vominhhau5@gmail.com>
2023-01-14 18:03:32 +0700

    thay doi text

---
+++ AI.py
@@ -0,0 +1,14 @@
+import speech_recognition as sr
+#initialize the recognizer
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    #read the audio data from the default microphone
+    audio_data = r.record(source,duration=5)
+    print("Chờ chút nhé...")
+    # convert speech to text
+    try:
+        text = r.recognize_google(audio_data, language="vi")
+    except:
+        text = "Xin lỗi bạn hãy nói lại !"
+
+    print(text)
